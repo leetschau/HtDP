b4cafa363c0c14371f15a771ca39a56810e27b04
Li Chao <leetschau@gmail.com>
2019-11-10 22:24:43 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Intro
+
+This repo contains my notes of the book *HtDP 2e*, short for
+[How to Design Programs, Second Edition](https://htdp.org/2019-02-24/index.html)
+by Matthias Felleisen, Robert Bruce Findler, Matthew Flatt, Shriram Krishnamurthi.
---
+++ prologue/circle.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname prologue) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+(place-image (circle 5 "solid" "green")
+             50 80
+             (empty-scene 100 100))
